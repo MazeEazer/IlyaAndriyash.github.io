323fb29c23a02708295ffa4dc8db368536da124f
IlyaAndriyash <Crimzzon@yandex.ru>
2024-10-06 20:54:43 +0300

    Add files via upload

---
+++ CALCULATOR/index.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const price = document.querySelector("#price-container");
+    const input = document.querySelector("#input-box");
+    const good = document.querySelector("#good");
+    let item;
+
+    const numberRegex = /^\d+$/; 
+
+    document.querySelector(".button").addEventListener("click", () => {
+        const inputValue = input.value.trim(); 
+
+        if (inputValue === "") {
+            if (!item) {
+                item = document.createElement("div");
+                item.classList.add("price__item");
+                price.appendChild(item);
+            }
+            item.textContent = "Ошибка: поле ввода не должно быть пустым.";
+        } else if (numberRegex.test(inputValue)) { 
+            const cost = +inputValue * +good.value;
+
+            if (!item) {
+                item = document.createElement("div");
+                item.classList.add("price__item");
+                price.appendChild(item);
+            }
+            item.textContent = `Cтоимость: ${cost} рублей`;
+        } else {
+            if (!item) {
+                item = document.createElement("div");
+                item.classList.add("price__item");
+                price.appendChild(item);
+            }
+            item.textContent = "Ошибка: введены неверные данные. Пожалуйста, введите число.";
+        }
+
+        input.value = ""; // Очищаем инпут после обработки
+    });
+});
